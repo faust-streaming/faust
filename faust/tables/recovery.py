13fe389811ef74f8b6014169de09722f1841674a
William Barnhart <william.barnhart@he360.com>
2022-07-15 13:40:12 -0400

    fix recovery linting

--- faust/tables/recovery.py
+++ faust/tables/recovery.py
@@ -472,7 +472,7 @@ class Recovery(Service):
                     self._set_recovery_ended()
 
                 # The changelog partitions are only in the active_tps set need to be resumed
-                active_only_partitions = (active_tps - standby_tps)
+                active_only_partitions = active_tps - standby_tps
                 if active_only_partitions:
                     T(consumer.resume_partitions)(active_only_partitions)
                     T(self.app.flow_control.resume)()
