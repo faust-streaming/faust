15776e0607023595fce01b686e200205aec06c04
William Barnhart <william.barnhart@he360.com>
2022-07-19 10:18:04 -0400

    add backup_partition and restore_backup to MyStore test class

--- tests/unit/stores/test_base.py
+++ tests/unit/stores/test_base.py
@@ -29,6 +29,19 @@ class MyStore(Store):
     def reset_state(self):
         ...
 
+    async def backup_partition(
+        self, tp, flush: bool = True, purge: bool = False, keep: int = 1
+    ) -> None:
+        ...
+
+    def restore_backup(
+        self,
+        tp,
+        latest: bool = True,
+        backup_id: int = 0,
+    ) -> None:
+        ...
+
 
 class Test_Store:
     @pytest.fixture
