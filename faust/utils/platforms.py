c8caba1fbcff8f2c7c71c1b4738b6fae8e86d95b
William Barnhart <william.barnhart@he360.com>
2022-09-19 17:56:50 -0400

    add b607 as well

--- faust/utils/platforms.py
+++ faust/utils/platforms.py
@@ -16,7 +16,7 @@ def max_open_files() -> Optional[int]:
             # macOS bash always returns infinity, even though there
             # is an actual system limit.
             if platform.system() == "Darwin":
-                output = subprocess.check_output(  # nosec: B603
+                output = subprocess.check_output(  # nosec: B603,B607
                     [
                         "sysctl",
                         "-q",
