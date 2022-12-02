1a4a5922a5e07a30848e1f86da9d5ee7eb5cc845
William Barnhart <william.barnhart@he360.com>
2022-12-02 16:59:02 -0500

    Readd Confluent Kafka drivers

--- faust/transport/drivers/__init__.py
+++ faust/transport/drivers/__init__.py
@@ -1,21 +1,15 @@
 """Transport registry."""
-from yarl import URL
+from typing import Type
+from mode.utils.imports import FactoryMapping
+from faust.types import TransportT
 
-from .aiokafka import Transport as AIOKafkaTransport
+__all__ = ['by_name', 'by_url']
 
-__all__ = ["by_name", "by_url"]
-
-
-DRIVERS = {
-    "aiokafka": AIOKafkaTransport,
-    "kafka": AIOKafkaTransport,
-}
-
-
-def by_name(driver_name: str):
-    return DRIVERS[driver_name]
-
-
-def by_url(url: URL):
-    scheme = url.scheme
-    return DRIVERS[scheme]
+TRANSPORTS: FactoryMapping[Type[TransportT]] = FactoryMapping(
+    aiokafka='faust.transport.drivers.aiokafka:Transport',
+    confluent='faust.transport.drivers.confluent:Transport',
+    kafka='faust.transport.drivers.aiokafka:Transport',
+)
+TRANSPORTS.include_setuptools_namespace('faust.transports')
+by_name = TRANSPORTS.by_name
+by_url = TRANSPORTS.by_url
