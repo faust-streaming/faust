5330c45c7fb9855b43b2f711ca9aa0ce2bdbbb06
Zev Isert <zevisert@users.noreply.github.com>
2024-03-01 09:19:59 -0800

    fix: serialize (named)tuple when using orjson (#612)

--- faust/utils/json.py
+++ faust/utils/json.py
@@ -69,7 +69,7 @@ except ImportError:  # pragma: no cover
 DECIMAL_MAXLEN = 1000
 
 #: Types that we convert to lists.
-SEQUENCE_TYPES: TypeTuple[Iterable] = (set, frozenset, deque)
+SEQUENCE_TYPES: TypeTuple[Iterable] = (set, frozenset, deque, tuple)
 
 DateTypeTuple = Tuple[Union[Type[datetime.date], Type[datetime.time]], ...]
 DatetimeTypeTuple = Tuple[
